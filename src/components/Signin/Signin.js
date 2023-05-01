ef0c3848f312f43c920a63bd331f9e9971b2e6ff
Jarno Ojasaar <jarno.ojasaar@gmail.com>
2023-05-01 03:42:04 +0300

    added render.com links

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -15,7 +15,7 @@ class Signin extends Component {
     this.setState({ signInPassword: event.target.value });
   };
   onSubmitSignIn = () => {
-    fetch("http://localhost:3000/signin", {
+    fetch("https://facedetect-api-backend.onrender.com/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
